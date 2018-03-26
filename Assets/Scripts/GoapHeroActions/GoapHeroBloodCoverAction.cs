959896c8393fe06aebf3a1f7ef60ac157159022c
havster09 <havster09@gmail.com>
2018-03-26 21:45:28 +1100

    b4 fucking it up

---
+++ Assets/Scripts/GoapHeroActions/GoapHeroBloodCoverAction.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.GoapHeroActions
+{
+    public class GoapNpcIsDestroyedResetAction : GoapHeroAction
+    {
+        public GoapNpcIsDestroyedResetAction()
+        {
+            addEffect("destroyEnemyReset", true);
+            addEffect("destroyEnemy", false);
+            DistanceToTargetThreshold = 1f;
+        }
+
+        public override void reset()
+        {
+            NpcIsDestroyedReset = false;
+            TargetNpcAttribute = null;
+        }
+
+        public override bool isDone()
+        {
+            return NpcIsDestroyedReset;
+        }
+
+        public override bool requiresInRange()
+        {
+            return false;
+        }
+
+        public override bool checkProceduralPrecondition(GameObject agent)
+        {
+            return true;
+        }
+
+        public override bool perform(GameObject agent)
+        {
+            if (HeroScript.NpcHeroAnimator.GetBool("heroBloodCover") == false)
+            {
+                HeroScript.NpcHeroAnimator.SetBool("heroBloodCover", true);
+                HeroScript.WaitFor(() =>
+                {
+                    HeroScript.NpcHeroAnimator.SetBool("heroBloodCover", false);
+                    NpcIsDestroyedReset = true;
+                }, 1f);
+            }
+            return NpcIsDestroyedReset;
+        }
+    }
+}
